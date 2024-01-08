064ebf48e1a7dd2255ccf4ace34efbf42b414e58
ramgopal <ramgopal.nannapaneni29@gmail.com>
2024-01-08 00:00:09 -0500

    logic-2 solutions  added

---
+++ Logic-2/loan_sum.py
@@ -0,0 +1,25 @@
+"""
+
+Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
+
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+"""
+def lone_sum(a, b, c):
+ if a==b==c:
+    return 0
+ elif a==b:
+    return c
+ elif a==c:
+     return b
+ elif b==c:
+    return a
+ elif a==b==c:
+    return 0
+ else:
+   return a+b+c
+  
+  
+    
